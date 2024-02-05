7026089435c71b5ee9a5408334ed9d83cf259206
Mike MacDonald <mikem@Mikes-MacBook-Pro.local>
2024-02-05 16:37:57 -0500

    use new style plugins

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
-module.exports = {
-  plugins: [
-	"syntax-async-functions",
-    "syntax-class-properties",
-    "syntax-flow",
-    "syntax-jsx",
-    "syntax-object-rest-spread",
-    "syntax-trailing-function-commas",
-  ]
-}
+module.exports = function preset(api, options) {
+  return {
+    plugins: [
+      "@babel/syntax-jsx",
+      "@babel/syntax-flow",
+      ...(!!options.flow && options.flow === 'strip' ? ['@babel/transform-flow-strip-types'] : []),
+      ...(!!options.flow && options.flow === 'comment' ? ['@babel/transform-flow-comments'] : []),
+      "@babel/syntax-class-properties",
+      "@babel/syntax-async-generators",
+      "@babel/syntax-optional-chaining",
+      "@babel/syntax-object-rest-spread",
+      "@babel/syntax-optional-catch-binding",
+      "@babel/syntax-nullish-coalescing-operator"
+    ],
+    overrides: [
+      {
+        test: $ => !!$ && ($.endsWith('.ts') || $.endsWith('.tsx')),
+        plugins: [
+          ['@babel/syntax-typescript', { isTSX: true }],
+        ],
+      },
+    ],
+  }
+};
